e9d6ecf821fed505d5ba38337be6b6a631321bef
Rajmohan R <ram.rajmohanr@gmail.com>
2020-08-07 06:57:01 +1000

    auto-commit from rajmohra@f8ffc2228ae2 on Fri  7 Aug 2020 06:57:01 AEST

--- gitpush.sh
+++ gitpush.sh
@@ -7,13 +7,23 @@ REPO_DIR=$(pwd)
 
 cd ${REPO_DIR}
 
+# pull rebase - remote master
+${GIT} checkout master
+${GIT} pull --all
 
 # stage & commit - local branch
 ${GIT} checkout local
-${GIT} pull --rebase
 ${GIT} add --all .
 ${GIT} commit -m "$message"
+
+# merge local branch with master
+${GIT} checkout master
+${GIT} merge local --no-edit
 
 # push changes to remote
 gitPush=$(${GIT} push -vvv origin master 2>&1)
 echo "$gitPush"
+
+
+# checkout to local branch
+${GIT} checkout local
